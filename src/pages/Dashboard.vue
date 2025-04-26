ff02262cf886251ea57f4e3a31b533be4e3e0a57
Kishi011 <viniciusmulinari.st@gmail.com>
2025-04-25 21:41:02 -0300

    mexendo no dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,66 +1,35 @@
 <template>
-  <v-main style="margin-left: 100px;">
-    <v-container class="lexend-font" max-width="800">
-      <h1 class="mb-5">Dashboard</h1>
-      <!-- Informações de Lucros do Mês -->
-      <v-row>
-        <v-col cols="12">
-          <v-card class="rounded-lg">
-            <v-card-title>Ganhos do Mês</v-card-title>
-            <v-card-text>
-              <div>
-                <v-row>
-                  <v-col class="d-flex align-center">
-                    <v-icon size="30" class="mr-2">mdi-cash</v-icon>
-                    <h2 class="d-inline">R$ 25.481</h2>
-                  </v-col>
-                </v-row>
-              </div>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-  
-      
-  
-      <!-- Serviços Prestados -->
-      <v-row>
-        <v-col cols="12">
-          <v-card class="rounded-lg">
-            <v-card-title>Serviços Prestados</v-card-title>
-            <v-card-text>
-              <v-card class="mr-2 pa-2 d-inline">Banho: 5</v-card>
-              <v-card class="mr-2 pa-2 d-inline">Tosa: 4</v-card>
-              <v-card class="mr-2 pa-2 d-inline">Vacinação: 2</v-card>
-              <v-card class="mr-2 pa-2 d-inline">Consulta: 3</v-card>
-              <v-card class="mr-2 pa-2 d-inline">Castração: 0</v-card>
-              <v-card class="mr-2 pa-2 d-inline">Cirurgia: 0</v-card>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-  
-      <!-- Gráfico de Vendas e Lucros -->
-      <v-row>
-        <v-col cols="12">
-          <v-card class="rounded-lg">
-            <v-card-title>Gráfico de Vendas e Lucros</v-card-title>
-              <v-card-text>
-                <BarChart />
-              </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-main>
+  <v-container class="lexend-font" max-width="800">
+    <h1 class="mb-5">Dashboard</h1>
+
+  <!-- Informações dos Ganhos do Mês -->
+    <h1 class="mb-5">Distribuição dos Serviços</h1>
+    <section class="d-flex justify-center">
+      <div class="d-flex flex-column align-center justify-center" style="margin-right: 200px;">
+        <h1>Ganhos do Mês</h1>
+        <h2 class="d-inline"><v-icon>mdi-cash</v-icon>R$ 25.481</h2>
+      </div>
+      <ServicesPieChart
+        style="max-width: 70%;"
+      />
+    </section>
+
+    <!-- Gráfico de Vendas e Lucros -->
+    <h1 class="mb-5">Gráfico de Vendas e Lucros</h1>
+    <BarChart
+      style="max-width: 100%;"
+    />
+  </v-container>
 </template>
 
 <script>
 import BarChart from '../components/BarChart.vue'
+import ServicesPieChart from '../components/ServicesPieChart.vue'
 export default {
   name: 'DashboardPage',
-  components: { BarChart }
+  components: { BarChart, ServicesPieChart }
 }
+
 </script>
 
 <style>
