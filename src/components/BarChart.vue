7e1769858be466e9806a3d2aa7c81656a99416da
felipe <callofnoob712@gmail.com>
2025-05-23 21:01:43 -0300

    Ref: Ajustando tela de dashboard

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,30 +1,57 @@
 <template>
-    <Bar
-      id="Vendas"
-      :options="chartOptions"
-      :data="chartData"
-    />
-  </template>
-  
-  <script>
-  import { Bar } from 'vue-chartjs'
-  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-  
-  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-  
-  export default {
-    name: 'BarChart',
-    components: { Bar },
-    data() {
-      return {
-        chartData: {
-          labels: [ 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro' ],
-          datasets: [ { label: 'Vendas', backgroundColor: '#1253e8',  data: [36, 22, 12, 15, 24, 19, 30, 11, 3, 18, 26, 14] } ]
-        },
-        chartOptions: {
-          responsive: true
-        }
+  <v-card>
+    <v-card-title>Gráfico de Vendas e Lucros</v-card-title>
+    <v-card-text :style="`height: ${height};`">
+      <Bar id="Vendas" :options="chartOptions" :data="chartData" />
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+
+  props: {
+    height: {
+      type: String,
+      default: '300px'
+    }
+  },
+
+  data() {
+    return {
+      chartData: {
+        labels: [
+          'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+          'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+        ],
+        datasets: [
+          {
+            label: 'Vendas',
+            backgroundColor: '#1253e8',
+            data: [36, 22, 12, 15, 24, 19, 30, 11, 3, 18, 26, 14]
+          }
+        ]
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false
       }
     }
   }
-  </script>
+}
+</script>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -2,19 +2,101 @@
   <v-container class="lexend-font" max-width="1000">
     <h1 class="mb-5">Dashboard</h1>
 
-  <!-- Informações dos Ganhos do Mês -->
     <h1 class="mb-5">Distribuição dos Serviços</h1>
-    <section class="d-flex justify-center">
-      <div class="d-flex flex-column align-center justify-center" style="margin-right: 200px;">
-        <h1>Ganhos do Mês</h1>
-        <h2 class="d-inline"><v-icon>mdi-cash</v-icon>R$ 25.481</h2>
-      </div>
-      <ServicesPieChart />
-    </section>
 
-    <!-- Gráfico de Vendas e Lucros -->
-    <h1 class="mb-5">Gráfico de Vendas e Lucros</h1>
-    <BarChart />
+    <v-row class="bg-red align-center">
+      <v-col md="8">
+        <v-row>
+          <v-col md="4" class="bg-green">
+            <v-card class="text-center">
+              <v-card-title class="text-h6">Total de agendamentos</v-card-title>
+              <v-card-text>
+                <h4>Mês anterior: 243</h4>
+                <h4>Mês atual: 243</h4>
+                <h4>Mês sucessor: 243</h4>
+              </v-card-text>
+            </v-card>
+          </v-col>
+
+          <v-col md="4">
+            <v-card class="text-center">
+              <v-card-title class="text-h6">Valores totais</v-card-title>
+              <v-card-text>
+                <h4>Recebimenos</h4>
+                <h4>Despesas</h4>
+                <h4>Lucros</h4>
+              </v-card-text>
+            </v-card>
+          </v-col>
+
+          <v-col md="4" class="bg-blue">
+            <v-card>
+              <v-card-title class="text-h6 text-center">Top melhores serviços</v-card-title>
+              <v-card-text>
+                <h4>Banho</h4>
+                <h4>Tosa</h4>
+                <h4>Vacina preventiva</h4>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+
+      <v-col md="4">
+        <v-card>
+          <v-card-title class="text-h6 text-center">Clientes</v-card-title>
+          <v-card-text>
+            <h4>Ativos: 786</h4>
+            <h4>Premium: 321</h4>
+            <h4>Inativos: 23</h4>
+          </v-card-text>
+        </v-card>
+      </v-col>
+
+      <v-col md="4">
+        <v-card class="text-center">
+          <v-card-title class="text-h6">Raças comuns</v-card-title>
+          <v-card-text>
+            <h4>Schitzu</h4>
+            <h4>Golden retriever</h4>
+            <h4>Pintscher</h4>
+          </v-card-text>
+        </v-card>
+      </v-col>
+
+      <v-col md="5">
+        <v-card class="text-center">
+          <v-card-title class="text-h6">Portes atendidos</v-card-title>
+          <v-card-text>
+            <h4>Pequeno</h4>
+            <h4>Médio</h4>
+            <h4>Filhote</h4>
+          </v-card-text>
+        </v-card>
+      </v-col>
+
+
+      <v-col md="12">
+        <v-card>
+          <v-card-title class="text-h6 text-center">Agendamentos marcados</v-card-title>
+          <v-card-text>
+            <h4>Hoje: </h4>
+            <h4>Amanhã: </h4>
+            <h4>Ontem: </h4>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row class="text-center">
+      <v-col md="6">
+        <ServicesPieChart />
+      </v-col>
+
+      <v-col md="6">
+        <BarChart />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -33,7 +115,7 @@ export default {
   },
 
   methods: {
-    onScroll () {
+    onScroll() {
       this.scrollInvoked.value++
     }
   },
@@ -42,10 +124,14 @@ export default {
 </script>
 
 <style>
-  .lexend-font {
-    font-family: "Lexend", sans-serif;
-    font-optical-sizing: auto;
-    font-weight: normal;
-    font-style: normal;
-  }
+.v-row {
+  margin: 0px !important;
+}
+
+.lexend-font {
+  font-family: "Lexend", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: normal;
+  font-style: normal;
+}
 </style>
